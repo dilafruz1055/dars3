37e52915c03b27608018bfe4f971bf64d2e43033
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-04-24 11:36:53 +0500

    dan

---
+++ index.js
@@ -0,0 +1,19 @@
+let taxminiySon1 = Math.floor(Math.random() * 10);
+let taxminiySon2 = Math.floor(Math.random() * 20);
+let taxminiySon3 = Math.floor(Math.random() * 100);
+
+console.log(taxminiySon1);
+console.log(taxminiySon1 * 2);
+console.log(taxminiySon1 / 2)
+console.log(taxminiySon1 % 2)
+
+console.log(taxminiySon2);
+console.log(taxminiySon2 * 2);
+console.log(taxminiySon2 / 2)
+console.log(taxminiySon2 % 2)
+
+
+console.log(taxminiySon3);
+console.log(taxminiySon3 * 2);
+console.log(taxminiySon3 / 2)
+console.log(taxminiySon3 % 2)
